d3f95831aaeaaf5c11c84b386518ab56e0e2afba
Jeel Savsani <jeel.savsani@jeel.savsani-C02G59R1ML7H>
2026-06-02 12:54:42 +0530

    profiling error

--- next.config.js
+++ next.config.js
@@ -12,13 +12,14 @@ const nextConfig = {
   images: {
     unoptimized: true, 
   },
+  turbopack: {},
   webpack: (config, { dev, isServer }) => {
     // This forces the React Profiler to stay enabled in your deployed production build
     if (!dev && !isServer) {
       config.resolve.alias = {
         ...config.resolve.alias,
         'react-dom$': 'react-dom/profiling',
-        'scheduler/tracing': 'scheduler/tracing-profiling',
+        // 'scheduler/tracing': 'scheduler/tracing-profiling',
       };
     }
     return config;
