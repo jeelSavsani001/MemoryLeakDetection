76f5c586fc4a1ed757e6cbccdb72a7cb21393a8f
Jeel Savsani <jeel.savsani@jeel.savsani-C02G59R1ML7H>
2026-06-03 12:11:23 +0530

    remove alias

--- next.config.js
+++ next.config.js
@@ -1,34 +1,18 @@
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   images: {
-//     unoptimized: true, 
-//   },
-// };
-
-// module.exports = nextConfig;
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
     unoptimized: true, 
   },
-  // turbopack: {},
   webpack: (config, { dev, isServer }) => {
-    // This forces the React Profiler to stay enabled in your deployed production build
     if (!dev && !isServer) {
-      config.resolve.alias = {
-        ...config.resolve.alias,
-        'react-dom$': 'react-dom/profiling',
-        // 'scheduler/tracing': 'scheduler/tracing-profiling',
-      };
-
-      // Disable minification for production builds
+      // Disable minification for MemLab
       config.optimization.minimize = false;
       config.optimization.minimizer = [];
-
-      // to help Webpack stabilize module IDs in non-minified builds
+      
+      // Preserve original names
       config.optimization.moduleIds = 'named';
       config.optimization.chunkIds = 'named';
+      config.optimization.mangleExports = false;
     }
     return config;
   },
