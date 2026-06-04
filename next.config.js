f00b341000e210b156846f5637aee40ed4287af8
Jeel Savsani <jeel.savsani@jeel.savsani-C02G59R1ML7H>
2026-06-04 14:32:24 +0530

    minification removed

--- next.config.js
+++ next.config.js
@@ -3,19 +3,6 @@ const nextConfig = {
   images: {
     unoptimized: true, 
   },
-  webpack: (config, { dev, isServer }) => {
-    if (!dev && !isServer) {
-      // Disable minification for MemLab
-      config.optimization.minimize = false;
-      config.optimization.minimizer = [];
-      
-      // Preserve original names
-      config.optimization.moduleIds = 'named';
-      config.optimization.chunkIds = 'named';
-      config.optimization.mangleExports = false;
-    }
-    return config;
-  },
 };
 
 module.exports = nextConfig;
